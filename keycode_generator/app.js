4a345ca2e958a7999adffe9623d88afc24bb87dc
Ibrahim <ibrahim.345xa@slmail.me>
2022-04-26 10:42:17 +0100

    styling changes

--- keycode_generator/app.js
+++ keycode_generator/app.js
@@ -7,7 +7,17 @@ const type = document.getElementById("whichInfo")
 
 document.addEventListener("keydown", (event) => {
     code.textContent = event.code;
-    key1.textContent = event.key;
+    key1.textContent = `${event.key} was pressed`;
     key2.textContent = event.key;
     type.textContent = event.which;
 });
+
+// other method
+// document.addEventListener('keypress', logKey);
+
+// function logKey(event) {
+//   code.textContent = ` ${event.code}`;
+//   key.textContent = ` ${event.key}`;
+//   which.textContent = ` ${event.which}`;
+//   keypress.textContent = ` ${event.which}`;
+// }
